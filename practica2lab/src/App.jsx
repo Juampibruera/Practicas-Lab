8e040b24282fefec21cb3c30b00a7004cdc58a0f
Juan Pablo Bruera <juanpibruera@gmail.com>
2024-04-26 23:06:34 -0300

    Update App.jsx

--- practica2lab/src/App.jsx
+++ practica2lab/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import Table from "./components/table/Table"
 
-function MyApp() {
-  const ingresosNetos = [
-    {marca: "McDonalds", ingreso: 1291283}, 
-    {marca: "Burger King", ingreso: 1927361}, 
-    {marca: "KFC", ingreso: 1098463}
+function App() {
+  const netIncomes = [
+    {brand: "McDonalds", income: 1291283}, 
+    {brand: "Burger King", income: 1927361}, 
+    {brand: "KFC", income: 1098463}
   ];
 
   return (
     <div>
-      <Table ingresosNetos={ingresosNetos} />
+      <Table netIncomes={netIncomes} />
     </div>
   );
 }
 
-export default MyApp;
+export default App;
