4e152f0e39ac00c1500323f9ff5d337998f73e83
Juan Pablo Bruera <juanpibruera@gmail.com>
2024-04-26 23:01:43 -0300

    Update Table.jsx

--- practica2lab/src/Table.jsx
+++ practica2lab/src/Table.jsx
@@ -1,5 +1,10 @@
-const Table = ({ netIncomes }) => {
+import PropTypes from "prop-types";
+
+const Table = ({netIncomes}) => {
+    const totalIncome = netIncomes.reduce((sum, item) => sum + item.income, 0);
+    const averageIncome = (totalIncome/netIncomes.length).toFixed(2);
     return (
+        <>
         <table>
             <thead>
                 <tr>
@@ -8,15 +13,21 @@ const Table = ({ netIncomes }) => {
                 </tr>
             </thead>
             <tbody>
-                {netIncomes.map((income, index) => (
+                {netIncomes.map((branch,index)=> (
                     <tr key={index}>
-                        <td>{income.brand}</td>
-                        <td>{income.income}</td>
+                        <td>{branch.brand}</td>
+                        <td>{branch.income}</td>
                     </tr>
                 ))}
             </tbody>
         </table>
+        <p>El promedio de ingresos de las marcas es: {averageIncome}</p>
+        </>
     );
 }
+        
+Table.propTypes = {
+    netIncomes: PropTypes.array
+};
 
 export default Table;
